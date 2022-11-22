7fab2ec4dc18513a35be6362308cb817a069549a
ydroulis <lombardi.droulis@gmail.com>
2022-11-21 21:00:27 -0300

    refac app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App tests', () => {
+  it('should render App', () => {
+    render(<App />);
+
+    const aplication = screen.getByTestId(/app/i);
+
+    expect(aplication).toBeInTheDocument();
+  });
+})
