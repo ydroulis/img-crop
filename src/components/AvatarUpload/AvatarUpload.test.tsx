66c923158a18e75a0131f7cb9437a42042da4f87
ydroulis <lombardi.droulis@gmail.com>
2022-11-21 21:12:58 -0300

    remove test with bug

--- src/components/AvatarUpload/AvatarUpload.test.tsx
+++ src/components/AvatarUpload/AvatarUpload.test.tsx
@@ -11,22 +11,4 @@ describe('AvatarUpload tests', () => {
 
     expect(avatarUpload).toBeInTheDocument();
   });
-  
-  it("should drop", async () => {
-    const file = new File(["hello"], "hello.geojson", {
-        type: "application/json"
-      });
-    
-      const { getByTestId } = render(<AvatarUpload />);
-    
-      const input = getByTestId("dropzone") as HTMLInputElement;
-    
-      act(() => {
-        userEvent.upload(input, file);
-      });
-    
-      expect(input.files[0]).toStrictEqual(file);
-      expect(input.files.item(0)).toStrictEqual(file);
-      expect(input.files).toHaveLength(1);
-  })
 })
